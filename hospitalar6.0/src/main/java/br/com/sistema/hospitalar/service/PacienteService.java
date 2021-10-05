93367e96a261c7004812c4e220f6909f05033ad4
devGiovanni95 <giovanni.devjava@hotmail.com>
2021-10-05 20:49:15 -0300

    implementando o PacienteService

--- hospitalar6.0/src/main/java/br/com/sistema/hospitalar/service/PacienteService.java
+++ hospitalar6.0/src/main/java/br/com/sistema/hospitalar/service/PacienteService.java
@@ -4,6 +4,7 @@ import br.com.sistema.hospitalar.entities.PacienteEntity;
 import br.com.sistema.hospitalar.repositories.PacienteRepository;
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -14,9 +15,29 @@ public class PacienteService {
     @Autowired
     private PacienteRepository pacienteRepository;
 
-    public PacienteEntity findById(Long id){
+    public PacienteEntity findById(Long id) {
         Optional<PacienteEntity> paciente = pacienteRepository.findById(id);
-       return paciente.orElseThrow(() -> new RuntimeException() );
+        return paciente.orElseThrow(() -> new RuntimeException());
     }
 
+    public PacienteEntity insert(PacienteEntity obj) {
+        obj.setId(null);
+        return pacienteRepository.save(obj);
+    }
+
+    public PacienteEntity update(PacienteEntity obj) {
+        findById(obj.getId());
+        return pacienteRepository.save(obj);
+    }
+
+    public void delete(Long id) throws DateIntegrityException {
+        findById(id);
+        try {
+            pacienteRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DateIntegrityException("Não é possível excluir uma Paciente ");
+
+        }
+
+    }
 }
